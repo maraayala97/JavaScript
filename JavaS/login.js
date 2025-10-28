d2ec07f247afa6ef550c1209ea2f2f72fc3354b2
Marcia Ayala <maraayala97@gmail.com>
2025-10-27 23:44:54 -0300

    Sumo login a la entrega

--- JavaS/login.js
+++ JavaS/login.js
@@ -1,18 +1,3 @@
-// Tomo el usuario y la contraseña y la guardo en el localStorage, y despues debo validar usuario y contraseña , si estas son correctas dejo pasar a index
-
-//  <div class="formulario">   
- //      <form action="" class="formulariologin">
- //           <div class="usuario">
-  //              <label for="username">Usuario:</label>
-  //              <input type="text" id="username" name="username" placeholder="Ingresa tu usuario">
-  //          </div>
-  //          <div class="contrasena">
-   //             <label for="password">Contraseña:</label>
-  //              <input type="password" id="password" name="password" placeholder="Ingresa tu contraseña">
-  //          </div> 
-  //          <button type="submit" class="botoningresar">Ingresar</button>      
-   //     </form>
-
     //Tomo lo que ingreso en el input
    let usuarioingresado = document.getElementById("username");
    let contrasenaingresada = document.getElementById("password");
